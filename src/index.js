9607a164f88c8fc7ee0a47927ccd6b24f718784a
Keitumetse-Sebothe <148775009+Keitumetse-Sebothe@users.noreply.github.com>
2024-06-03 17:34:11 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-function refreshWeather (response) {
-    let temperatureElement = document.querySelector ("#temperature");
-    let temperature = document.querySelector("#city");
-
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+  
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
-}
-
-function searchCity (city) {
-    let apiKey = "39aba29446903cbedo2db0ftec583c0e";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}';
+  }
+  
+  function searchCity(city) {
+    let apiKey = "39a6a2944b903cbedo2db0ftec583c0e";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
-
-    function handleSearchSubmit (event) {
-        event.preverntDefault ();
-        let searchInput = document.querySelector("#search-form-input");
-
-        searchCity(searchInput.value);
-    }
-}
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+  
+    searchCity(searchInput.value);
+  }
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-  searchCity ("Paris");
+  
+  searchCity("Paris");
