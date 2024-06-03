32e4c9a6d677ef6c25d8721f865beab5fd36b8c1
Keitumetse-Sebothe <148775009+Keitumetse-Sebothe@users.noreply.github.com>
2024-06-03 17:32:34 +0200

    Updated index.js api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-  }
+function refreshWeather (response) {
+    let temperatureElement = document.querySelector ("#temperature");
+    let temperature = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity (city) {
+    let apiKey = "39aba29446903cbedo2db0ftec583c0e";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}';
+    axios.get(apiUrl).then(refreshWeather);
+
+    function handleSearchSubmit (event) {
+        event.preverntDefault ();
+        let searchInput = document.querySelector("#search-form-input");
+
+        searchCity(searchInput.value);
+    }
+}
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+  searchCity ("Paris");
